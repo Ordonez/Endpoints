258edb97d16c5c2b666bebf24411ca49e037f535
Eric Ordonez <jwustudent@gmail.com>
2020-08-20 03:11:19 -0500

    Edited get mapping code

--- src/main/java/com/SpringTraining/Endpoints/Controllers/RestfulController.java
+++ src/main/java/com/SpringTraining/Endpoints/Controllers/RestfulController.java
@@ -1,6 +1,5 @@
 package com.SpringTraining.Endpoints.Controllers;
 
-import com.SpringTraining.Endpoints.Restful;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +14,10 @@ public class RestfulController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/restful")
-    public Restful restful(@RequestParam(value = "The Daily Restful Services Training", defaultValue = "World") String name) {
-        return new Restful(counter.incrementAndGet(), String.format(template, "The Daily Restful Services Training"));
+    public com.SpringTraining.Endpoints.entity.Restful restful(
+            @RequestParam(value = "The Daily Restful Services Training", defaultValue = "World") String name) {
+        return new com.SpringTraining.Endpoints.entity.Restful(counter.incrementAndGet(),
+                String.format(template, "The Daily Restful Services Training"));
     }
 }
 
